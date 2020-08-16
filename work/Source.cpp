ae464d216e0af9bcb62ca65fdb41c16fc5636b1c
channasorn chungmarisakul <63010185@KMITL.AC.TH>
2020-08-16 23:10:02 +0700

    1

---
+++ work/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+float allweight();
+float numP[6];
+float sum;
+int i = 0;
+int main() {
+	allweight();
+	printf("allweigth : ");
+	printf("%f", sum);
+	return 0;
+	
+	}
+float allweight() {
+	
+	for (i = 0; i < 6; i++)
+	{
+		printf("weight #");
+		printf("%d ", i + 1);
+		scanf("%f", &numP[i]);
+		printf("\n");
+		sum += numP[i];
+		if (sum > 450) {
+			printf("O V E R W E I G H T\n");
+			sum = sum - numP[i];
+			i = i - 1;
+		}
+		
+	}
+	return sum;
+
+}
